0a68cdebd4e7f3a3d36a4fcf552da1c58de4acc9
Debankur Ghosh <ghosh_debankur@yahoo.com>
2021-04-22 12:24:21 +0530

    final commit

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import numpy as np
+"""import numpy as np"""
 import cv2 
 import os
 from flask import Flask, render_template, Response, request, send_file, redirect
-from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
-sub = cv2.createBackgroundSubtractorMOG2()  # create background subtractor
-
+sub = cv2.createBackgroundSubtractorMOG2() 
+#creates an instance directory for uploading files (/instance/upload)
 os.makedirs(os.path.join(app.instance_path, 'upload'), exist_ok=True)
-@app.route('/')
+
+@app.route('/') 
 def index():
-    """Video streaming home page."""
+    
     return render_template('index.html')
 
 @app.route('/webcam')
@@ -18,7 +19,7 @@ def webcam():
     return render_template('webcam.html')
 
 
-@app.route('/image')#, methods =['GET','POST']) 
+@app.route('/image') 
 def image_page():
     return render_template('image.html')
 
@@ -38,24 +39,19 @@ def upload():
 def __del__():
     cam.stop()
 
-"""def get_img():
-    if request.method == 'POST':
-        fil = request.files['file']
-        fil.save(secure_filename(fil.filename))
-    return send_file(fil, mimetype='image/gif')"""
 
 
-def gen():
+def gen():    #function for etecting faces on video and webcam
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-    #eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_eye.xml')
+    
 
-    cam=cv2.VideoCapture(0)
-    while(cam.isOpened()):
+    cam=cv2.VideoCapture(0)   #Cahnge VodioCapture(0) TO VideoCapture(1) if video is not working
+    while(cam.isOpened()):    #Change VideoCapture index to 1 incase if video is not working 
 
         ret, frame=cam.read()
 
         if not ret:
-            frame= cv2.VideoCapture(0)
+            frame= cv2.VideoCapture(0) #Cahnge VodioCapture(0) TO VideoCapture(1) if video is not working
             continue
         if ret:
 
@@ -67,30 +63,24 @@ def gen():
                 image = cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = image[y:y+h, x:x+w]
-                #eyes = eye_cascade.detectMultiScale(roi_gray)
-                #for (ex,ey,ew,eh) in eyes:
-                    #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                
+                
 
-    #cv2.imshow("countours", image)
+    
         frame = cv2.imencode('.jpg', image)[1].tobytes()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    #time.sleep(0.1)
+    
         key = cv2.waitKey(20)
         if key == 27:
             break
-"""def get_image():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-    return f """
 
-def gen2():
+
+def gen2():  #function for detecting faces on images
     
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
     
     frame = cv2.imread('instance/upload/file1')
-    #imge= get_image()
-    #frame = cv2.imread(imge)
+        
     image= cv2.resize(frame, (0, 0), None, 1, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -98,73 +88,26 @@ def gen2():
         image = cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = image[y:y+h, x:x+w]
-        #eyes = eye_cascade.detectMultiScale(roi_gray)
-            #for (ex,ey,ew,eh) in eyes:
-                #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        
+            
+            
 
-    #cv2.imshow('img',img)
+    
     frame = cv2.imencode('.jpg', image)[1].tobytes()
     return (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
 
-    """Video streaming generator function."""
-    """cap = cv2.VideoCapture(2)
-
-    # Read until video is completed
-    while(cap.isOpened()):
-        ret, frame = cap.read()  # import image
-        if not ret: #if vid finish repeat
-            frame = cv2.VideoCapture(2)
-            continue
-        if ret:  # if there is a frame continue with code
-            image = cv2.resize(frame, (0, 0), None, 1, 1)  # resize image
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converts image to gray
-            fgmask = sub.apply(gray)  # uses the background subtraction
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))  # kernel to apply to the morphology
-            closing = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
-            opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
-            dilation = cv2.dilate(opening, kernel)
-            retvalbin, bins = cv2.threshold(dilation, 220, 255, cv2.THRESH_BINARY)  # removes the shadows
-            contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            minarea = 400
-            maxarea = 50000
-            for i in range(len(contours)):  # cycles through all contours in current frame
-                if hierarchy[0, i, 3] == -1:  # using hierarchy to only count parent contours (contours not within others)
-                    area = cv2.contourArea(contours[i])  # area of contour
-                    if minarea < area < maxarea:  # area threshold for contour
-                        # calculating centroids of contours
-                        cnt = contours[i]
-                        M = cv2.moments(cnt)
-                        cx = int(M['m10'] / M['m00'])
-                        cy = int(M['m01'] / M['m00'])
-                        # gets bounding points of contour to create rectangle
-                        # x,y is top left corner and w,h is width and height
-                        x, y, w, h = cv2.boundingRect(cnt)
-                        # creates a rectangle around contour
-                        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                        # Prints centroid text in order to double check later on
-                        cv2.putText(image, str(cx) + "," + str(cy), (cx + 10, cy + 10), cv2.FONT_HERSHEY_SIMPLEX,.3, (0, 0, 255), 1)
-                        cv2.drawMarker(image, (cx, cy), (0, 255, 255), cv2.MARKER_CROSS, markerSize=8, thickness=3,line_type=cv2.LINE_8)
-        #cv2.imshow("countours", image)
-        frame = cv2.imencode('.jpg', image)[1].tobytes()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        #time.sleep(0.1)
-        key = cv2.waitKey(20)
-        if key == 27:
-           break"""
-    
-   
         
 
-@app.route('/video_feed')
+@app.route('/video_feed') #sending video feed
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/image_feed')
+@app.route('/image_feed') #sending image feed
 def image_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen2(),
